15bc690b1eb14724865cd61bbdcb9c98591aebc6
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-08 12:23:59 -0400

    Refactor test.js and test.ts: update weather variable declaration and improve getWeather function

--- typescript-practice/test.js
+++ typescript-practice/test.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const calculate = (a, b) => a + b;
 console.log(calculate(40, 10));
-//declare variable
-let weather = "sunny";
-function getWeather() {
-    const weather = "sunny";
+//declare variable test
+const weather = "sunny";
+function getWeather(weather) {
+    ;
     return weather;
 }
+getWeather(weather);
 //# sourceMappingURL=test.js.map
+
--- typescript-practice/test.ts
+++ typescript-practice/test.ts
@@ -1,10 +1,28 @@
 const calculate = (a: number, b: number) => a + b;
 console.log(calculate(40, 10));
 
-//declare variable
-let weather: string = "sunny"; 
+//declare variable test
+const weather = "sunny";
 
-// function getWeather(): string {
-//     const weather = "sunny";
-//     return weather;
-// }
+function getWeather(weather: string): string {;
+    return weather;
+}
+
+getWeather(weather);
+
+//JS data types with TS type annotation 
+let stringType: string = "string";
+let booleanType: boolean = true;
+let intType: number = 45;
+let floatType: number = 34.5;
+let nullType: null = null;
+let undefiniedType: undefined = undefined;
+
+//Union type: variables/parameters that have more than one data type
+let stringOrNumber: string | number;
+stringOrNumber = "string";
+stringOrNumber = 23;
+
+//Array Type
+let numberArray: number[] = [];
+numberArray.push(1);
