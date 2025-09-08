d59163724c2d0c39b8c023023d51f9c6a55444fb
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-08 11:32:29 -0400

    Refactor index.js: replace require with import statements and implement API routes

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,9 +1,24 @@
-const express = require('express'); //load express package into file 
+import { routeHello, routeAPINames } from "./routes.js";
+import express from "express";
+
 const server = express(); //instantiate application 
+//const express = require('express'); //load express package into file 
 const port = 3000; //constant for the port to use 
 
 server.get('/hello', function (req, res){
-    res.send('First Program String');
+    const response = routeHello(req, res);
+    res.send(response);
+});
+
+server.get("/api/names", async function (req, res) {
+    let response;
+    try {
+        response = await routeAPINames(req, res);
+    } catch (err) {
+        console.log(err);
+    }
+    // send back the fetched response (or an error message)
+    res.send(response);
 });
 
 server.listen(port, function () {
