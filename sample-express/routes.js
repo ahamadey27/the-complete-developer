c52bb1b5590442dddc3f3a3739ffcd4113af10a2
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-08 11:19:27 -0400

    finished comments for route.js

--- sample-express/routes.js
+++ sample-express/routes.js
@@ -1,8 +1,9 @@
 import fetch from  "node-fetch"; //import fetch module for async requests 
 
-const routeHello = () => "Hello Fetch";
+const routeHello = () => "Hello Fetch"; //create first for existing /hello endpoint
 
-const routeAPINames = async () => {
+//This route will create an API endpoint page that will list usernames and IDs
+const routeAPINames = async () => { //define an async function so we can use await syntax for fetch call
     const url = "https://www.usemodernfullstack.dev/api/v1/users";
     let data;
     try {
@@ -12,11 +13,12 @@ const routeAPINames = async () => {
     catch (err) {
         return err;
     }
-    const names = data
-        .map((item) => `id: ${item.id}, name: ${item.name}`)
+    //define APO endpoint in constant and another variable to store async data (data is scoped within try catch block)
+    const names = data 
+        .map((item) => `id: ${item.id}, name: ${item.name}`) //.map iterates over data to display in JSON if succeeds 
 
-        .join("<br>");
+        .join("<br>"); //join all items with a break tag
     return names;
 };
 
-export {routeHello, routeAPINames}
+export {routeHello, routeAPINames} //export data under named export 
