b1827a30ee8cb642d476765245e9459915bcf511
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-17 17:14:30 -0400

    refactor: update App component to display ToDo List and remove unused elements

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,11 +6,9 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
-      <h2>More text</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quae ut voluptatibus debitis saepe eveniet veritatis hic dolor placeat rerum, deserunt facilis excepturi magni earum quo nemo quibusdam blanditiis. Nulla.</p> 
+      <h1>My ToDo List</h1>
     </>
-  )
+  );
 }
 
 export default App
