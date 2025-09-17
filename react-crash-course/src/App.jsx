7900bd90c82c1f3165e79e641d956a10a963ff8c
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-17 17:25:19 -0400

    feat: add Todo component and update App to display ToDo items

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -7,6 +7,23 @@ function App() {
   return (
     <>
       <h1>My ToDo List</h1>
+      <div>
+        <h2>Finish This React Course</h2>
+        <span>...</span>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Finish ASAP Frontend</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Land a job</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>earn money</h2>
+        <button>Delete</button>
+      </div>
     </>
   );
 }
