086e1a8af8fc3a7e83abc28154bf26a9a1877a6c
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-19 13:07:17 -0400

    refactor: replace manual popup toggle with useState for better state management

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import TodoTitle from "./components/TodoTitle.jsx"
 import Todo from "./components/Todo.jsx";
 import Popup from './components/popup.jsx';
+import {useState} from "react";
 
 function App() {
-  let popupOpen = false;
-
-  function togglePopup() {
-    popupOpen = !popupOpen;
-    console.log(popupOpen);
-  }
+  const [popupOpen, setPopupOpen] = useState(false);
+  
    
   return (
     <>
@@ -18,17 +15,17 @@ function App() {
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }}/> 
-        <button onClick={() => togglePopup()}>Add To</button> 
+        <button onClick={() => setPopupOpen(true)}>Add To</button> 
       </div>
       <Todo task="Learn React" 
       />
       <Todo task="Finish ASAP Frontend"
       />
       <Todo task="Land a job"
-      />
+      /> 
       <Todo task="Make money"
       />
-      {popupOpen ? <Popup buttonTitle="Are You Sure?"/> : null }
+      {popupOpen && <Popup buttonTitle="Are You Sure?"/>}
     </>
   );
 }
