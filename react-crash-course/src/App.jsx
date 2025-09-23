4a527bdd85b209e4ca33be3255b9a47bf9182835
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-23 19:48:09 -0400

    refactor: remove unused components and restructure App component for routing

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,44 +1,25 @@
 import './App.css'
-import TodoTitle from "./components/TodoTitle.jsx"
-import Todo from "./components/Todo.jsx";
-import Popup from './components/Popup.jsx';
-import Counter from "./components/Counter.jsx"
-import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home"
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() { 
-  //Best practice is to use useState hook on top, followed by functions then useEffect hook on the bottom
-    const [popupOpen, setPopupOpen] = useState(false);
-
-    //First step to Emitting Events 
-    function togglePopup() {
-      setPopupOpen(true)
-      console.log('parent notified')
-    }
-
-    function closePopup() {
-      setPopupOpen(false);
-    }
-
-    useEffect(() => {console.log('Only when component mounts')}, [])
-    useEffect(() => {console.log(`When component mounts AND ${popupOpen} changes`)}, [popupOpen])
-    useEffect(() => {console.log("on EVERY render")})
-   
   return (
-    <>
-      <TodoTitle todoTitle="My Todo's" />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
       <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }}/> 
-        <button onClick={() => setPopupOpen(true)}>Add To</button> 
+
       </div>
-      <Todo togglePopup ={togglePopup} task="Learn React"/>
-      <Todo togglePopup ={togglePopup} task="Finish ASAP Frontend"/>
-      <Todo togglePopup ={togglePopup} task="Land a job"/> 
-      <Todo togglePopup ={togglePopup} task="Make money"/>
-      {popupOpen && <Popup closePopup={closePopup} buttonTitle="Are You Sure?"/>}
-    </>
+
+    </Router>
+    
   );
 }
 
 export default App
+ 
