ba6b093de2adc6b580b2bf652ec53b7a6ed8769d
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-23 11:36:57 -0400

    feat: add click handlers for Increment, Decrement, and Reset buttons in Counter component

--- react-crash-course/src/components/Counter.jsx
+++ react-crash-course/src/components/Counter.jsx
@@ -1,9 +1,13 @@
+import {useState} from 'react';
+ 
 function Counter() {
+    const [count, setCount] = useState(0)
     return(
         <div>
-            <h1>Counter: 0</h1>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => console.log('increment')} >Increment</button>
+            <button onClick={() => console.log('decrement')}>Decrement</button>
+            <button onClick={() => console.log('reset')}>Reset</button>
         </div>
     )
 }
