6acdd415d2e42362d606d3b95ebec898e511b876
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-23 12:00:22 -0400

    fix: update Increment and Decrement button handlers to use functional updates for state management

--- react-crash-course/src/components/Counter.jsx
+++ react-crash-course/src/components/Counter.jsx
@@ -5,8 +5,8 @@ function Counter() {
     return(
         <div>
             <h1>Counter: {count}</h1>
-            <button onClick={() => setCount(count + 1)} >Increment</button>
-            <button onClick={() => setCount(count -1 )}>Decrement</button>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
+            <button onClick={() => setCount((prevCount)=> prevCount -1 )}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
         </div>
     )
