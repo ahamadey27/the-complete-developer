418ea310a98bc57cc3d4974e89dba11e7f47565d
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-24 11:12:12 -0400

    feat: add Nav component with navigation links and integrate into App component

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home"
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import Posts from './Pages/Posts'
+import Nav from './components/Nav';
 
 function App() { 
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />}/>
+        <Route path="/posts" element={<Posts />} />
       </Routes>
       <div>
 
