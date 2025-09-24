e0ebbab2d5510b946a3976bb2b6edaa90717eb1c
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-24 11:49:38 -0400

    feat: implement data fetching in Home component

--- react-crash-course/src/Pages/Home.jsx
+++ react-crash-course/src/Pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function Home() {
+    const [data, setData] = useState([])
+    
+    useEffect(() => { 
+        async function fetchData() {
+            const { data } = console.log(axios.get("https://jsonplaceholder.typicode.com/users"))
+            console.log(data)
+        }
+        fetchData()
+    }, [])
     return <h1>Home</h1>
 }
 
