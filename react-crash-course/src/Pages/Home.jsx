64c938ff936cf831d29523119ea1caedb3c875d1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-24 12:04:23 -0400

    feat: update Home component to fetch and display user data

--- react-crash-course/src/Pages/Home.jsx
+++ react-crash-course/src/Pages/Home.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 function Home() {
-    const [data, setData] = useState([])
-    
-    useEffect(() => { 
+    const [users, setUsers] = useState([])
+
         async function fetchData() {
-            const { data } = console.log(axios.get("https://jsonplaceholder.typicode.com/users"))
-            console.log(data)
-        }
-        fetchData()
-    }, [])
-    return <h1>Home</h1>
+            const { data } = await axios.get(
+                "https://jsonplaceholder.typicode.com/users");
+
+            setUsers(data)
+        };
+
+        useEffect(() => {
+            fetchData();
+        }, []);
+  
+    return <h1>{users.length > 0 ? users[0].username: null}</h1>
 }
 
-export default Home 
+export default Home  
